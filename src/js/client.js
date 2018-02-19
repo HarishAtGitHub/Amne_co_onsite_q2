7689780fbbbaea525f7fd942ac19a9f6bdb37b29
HarishAtGitHub <harish.pmprk@gmail.com>
2018-02-19 15:02:12 -0800

    adding router provision to route to components using path

--- src/js/client.js
+++ src/js/client.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 import {createStore,applyMiddleware, compose} from "redux";
 import thunk from "redux-thunk";
+import {Router} from "react-router";
 
 import Layout from "./components/common/Layout";
 import rootReducer from "./reducers/rootReducer";
+import routes from "./routes";
 
 let app = document.getElementById("app");
 let store = createStore(
@@ -17,7 +19,7 @@ let store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-        <Layout/>
+        <Router routes={routes}/>
     </Provider>,
     app
 );
---
+++ src/js/routes.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {Route} from "react-router";
+
+import Layout from "./components/common/Layout";
+
+export default (
+    <Route path="/" components={Layout}>
+    </Route>
+)
