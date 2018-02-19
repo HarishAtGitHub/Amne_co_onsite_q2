ac5509bc4d8724f5b6c262f7d8b66fa11555eee6
HarishAtGitHub <harish.pmprk@gmail.com>
2018-02-19 14:51:40 -0800

    basic react js with a simple component

---
+++ src/js/components/common/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Layout extends React.Component {
+    render() {
+        return (
+            <div>Layout works !</div>
+        )
+    }
+}
+
+export default Layout;
