7fb132484a2e1d149e9594b7302a2194bb427f85
HarishAtGitHub <harish.pmprk@gmail.com>
2018-02-19 15:39:07 -0800

    making one google places auto complete work as per documentation

--- src/js/components/realestate/RealEstateAgencyLocatorForm.js
+++ src/js/components/realestate/RealEstateAgencyLocatorForm.js
@@ -1,15 +1,30 @@
 import React from "react";
+import PlacesAutocomplete from 'react-places-autocomplete';
 
 class RealEstateAgencyLocatorForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { address: 'San Francisco, CA' }
+    }
+
+    onChange(address) {
+        this.setState({ address });
+    }
+
     render() {
+        const inputProps = {
+            value: this.state.address,
+            onChange: this.onChange.bind(this)
+        };
+
         return (
             <form>
                 <h4 className="form-header"> Real Estate  Agency Locator </h4>
                 <div className="form-group">
-                    <input type="text" className="form-control"/>
+                    <PlacesAutocomplete className="form-control" inputProps={inputProps}/>
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control"/>
+                    {/*<PlacesAutocomplete className="form-control"/>*/}
                 </div>
                 <div className="form-group">
                     <input type="submit" className="btn btn-lg btn-block btn-success"/>
--- src/js/components/realestate/RealEstateAgencyLocatorPage.js
+++ src/js/components/realestate/RealEstateAgencyLocatorPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import RealEstateAgencyLocatorForm from "./RealEstateAgencyLocatorForm";
 
 class RealEstateAgencyLocatorPage extends React.Component {
