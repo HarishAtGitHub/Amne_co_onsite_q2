353d29e37002d2f21a8c5e7db0876f305ef1be30
HarishAtGitHub <harish.pmprk@gmail.com>
2018-02-19 14:58:44 -0800

    making app debuggable through browser redux extension

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
-import {createStore,applyMiddleware} from "redux";
+import {createStore,applyMiddleware, compose} from "redux";
 import thunk from "redux-thunk";
 
 import Layout from "./components/common/Layout";
@@ -10,7 +10,9 @@ import rootReducer from "./reducers/rootReducer";
 let app = document.getElementById("app");
 let store = createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    compose(applyMiddleware(thunk),
+        window.devToolsExtension? window.devToolsExtension(): f=>f)
+
 )
 
 ReactDOM.render(
