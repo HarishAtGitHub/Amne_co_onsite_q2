7827fae8f356196d89224002521e592303a77c0e
HarishAtGitHub <harish.pmprk@gmail.com>
2018-02-19 14:56:55 -0800

    making provision for react to talk to redux statemanger store through provider

--- src/js/client.js
+++ src/js/client.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import {createStore,applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+
 import Layout from "./components/common/Layout";
+import rootReducer from "./reducers/rootReducer";
 
 let app = document.getElementById("app");
+let store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+)
 
 ReactDOM.render(
-    <Layout/>,
+    <Provider store={store}>
+        <Layout/>
+    </Provider>,
     app
 );
