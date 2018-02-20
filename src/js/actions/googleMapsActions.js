a9baec57d6876142c389ded32261a0d32079eacc
HarishAtGitHub <harish.pmprk@gmail.com>
2018-02-20 06:40:00 -0600

    removing unnecessary console logs

--- src/js/actions/googleMapsActions.js
+++ src/js/actions/googleMapsActions.js
@@ -2,15 +2,11 @@ import {geocodeByAddress, getLatLng} from "react-places-autocomplete";
 
 export function getNearestNeighbors(address, placetype, radius, mapref) {
     return (dispatch) => {
-        console.log(address);
         return getAddressListAsLatLngs(address)
             .then((latLngs) => {
                 let results = []
                 for(let j =0; j< latLngs.length; j++) {
-                    console.log(j);
-                    console.log(latLngs[j])
                     let neighbors= nearbySearch(latLngs[j], placetype, radius, mapref);
-                    console.log(neighbors);
                     results.push(neighbors);
                 }
                 return results;
@@ -19,7 +15,6 @@ export function getNearestNeighbors(address, placetype, radius, mapref) {
 }
 
 export function getAddressListAsLatLngs(address) {
-    console.log(address);
     let latLngPromises = []
     for(let i = 0; i < address.length; i++) {
         latLngPromises.push(getLatLngFromAddress(address[i]))
@@ -35,9 +30,6 @@ export function getLatLngFromAddress(address) {
 }
 
 export function nearbySearch(loc, placetype, radius, mapdiv) {
-    console.log(loc.lat);
-    console.log(loc.lng);
-    console.log(placetype);
     let center = new google.maps.LatLng(loc.lat, loc.lng);
 
     let map = new google.maps.Map(mapdiv, {
@@ -64,8 +56,6 @@ export function nearbySearch(loc, placetype, radius, mapdiv) {
 }
 
 export function computeDistanceBetweenLocations(loc1, loc2) {
-    //console.log(loc1);
-    //console.log(loc2);
     return google.maps.geometry.spherical.computeDistanceBetween(
         new google.maps.LatLng(loc1.lat, loc1.lng),
         new google.maps.LatLng(loc2.lat, loc2.lng)
@@ -79,15 +69,11 @@ export function setSumDistanceFromPts(locations, points) {
             lng:locations[j].geometry.location.lng()
         };
         let sumDistance = 0;
-        console.log("********************")
         for(let i = 0; i < points.length; i++) {
             let currentPoint = points[i];
             let temp = computeDistanceBetweenLocations(currentLocation, currentPoint);
-            console.log(temp);
             sumDistance += temp;
         }
-        console.log("sum " + sumDistance);
         locations[j].sumDistance = sumDistance;
-        console.log(locations[j].sumDistance);
     }
 }
--- src/js/reducers/locationsReducer.js
+++ src/js/reducers/locationsReducer.js
@@ -3,7 +3,6 @@ import {SET_LOCATIONS} from "../actions/types"
 export default ((state={}, action={}) => {
     switch(action.type) {
         case SET_LOCATIONS:
-            console.log(action.locations)
             return action.locations
 
         default:
